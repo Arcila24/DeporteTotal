fc8fd574d884148150922906620efa8f590231b1
Arcila24 <121474659+Arcila24@users.noreply.github.com>
2024-12-03 16:05:27 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,11 @@
 <?php
 // Obtén las credenciales de la base de datos desde variables de entorno
-$host =$_env('host');        // Dirección del servidor de base de datos
-$username =$_env('username');    // Usuario de la base de datos
-$password =$_env('password');// Contraseña de la base de datos
-$dbname =$_env('dbname');      // Nombre de la base de datos
-$port =$_env('port');        // Puerto de la base de datos
+$DB_HOST=$_ENV["DB_HOST"];        // Dirección del servidor de base de datos
+$DB_USER=$_ENV["DB_USER"];    // Usuario de la base de datos
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];// Contraseña de la base de datos
+$DB_NAME=$_ENV["DB_NAME"];      // Nombre de la base de datos
+$DB_PORT=$_ENV["DB_PORT"];        // Puerto de la base de datos
 
 // Crear la conexión
-$conn = new mysqli($host, $username, $password, $dbname, $port);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error al conectar a la base de datos: " . $conn->connect_error);
-}
-
-echo "Conexión exitosa a la base de datos.";
+$db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
 ?>
